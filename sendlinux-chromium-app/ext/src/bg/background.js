879ab18d3346f14eeb9ecfb2a6529f6b38079e2c
Ole Algoritme <olealgoritme@gmail.com>
2019-10-30 21:55:46 +0100

    updated chromium extension

--- sendlinux-chromium-app/ext/src/bg/background.js
+++ sendlinux-chromium-app/ext/src/bg/background.js
@@ -1,16 +1,38 @@
-//message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+var onDisconnected = function() {
+    console.log("disconnected");
+}
+
+var onNativeMessage = function(msg) {
+    console.log("got msg: " + msg)
+}
 
 
-var port = chrome.runtime.connectNative('mpv');
-port.onMessage.addListener(function(msg) {
-  console.log("Received" + msg);
+function connect() {
+    port = chrome.runtime.connectNative('sendlinux');
+    port.onMessage.addListener(onNativeMessage);
+    port.onDisconnect.addListener(onDisconnected);
+}
+
+chrome.runtime.onMessage.addListener(function(data, sender) {
+    console.log("got msg: " + data);
+    if (data.length > 0) {
+        //connect();
+        chrome.runtime.sendNativeMessage('sendlinux',
+            { text: " https://www.youtube.com/watch?v=0b46E4mp_V8i"},
+            function(response) {
+                console.log("Received " + response);
+            });
+    }
 });
-port.onDisconnect.addListener(function() {
-  console.log("Disconnected");
-});
-port.postMessage({ text: " https://www.youtube.com/watch?v=y3h02BDPMJw" });
+
+/*
+ *
+ *var port = chrome.runtime.connectNative('mpv');
+ *port.onMessage.addListener(function(msg) {
+ *  console.log("Received" + msg);
+ *});
+ *port.onDisconnect.addListener(function() {
+ *  console.log("Disconnected");
+ *});
+ *port.postMessage({ text: " https://www.youtube.com/watch?v=y3h02BDPMJw" });
+ */
